53f82369add39b760ec8f7980747ba4371c2814e
WebWH <rbswkd113@naver.com>
2020-05-15 05:22:38 +0900

    Add Temporary Storage & Add Form Comment

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 # urls.py 에서는 views.py 호출하여 웹에서의 path(path함수 첫 인자)에 render 할 html 템플릿을 찾음
 # path(웹에서 보여지는 URL, html을 URL에 맵핑하기 위해 호출할 함수, a 태그 url과 매핑할 속성)
@@ -9,4 +10,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'), # path('웹에서의 path', views.py 내 post_detail() 찾아감, <a href="{% url 'post_detail' pk=post.pk %}">)
     path('post/new', views.post_new, name='post_new'), # 새 글 작성 # views.py post_new() 호출 # 화면에서 a 태그에 url 'post_new'를 지정했기 때문에 name='post_new' 을 보고 찾아옴
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'), # 글 수정 # views.py post_edit(request, pk) 2 인자를 넣어 호출
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'), # 임시저장
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'), # 글 게시
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'), # 글 삭제
 ]
--- blog/views.py
+++ blog/views.py
@@ -21,31 +21,62 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
     # C:\django\blog\templates\blog\post_detail.html
 
+
 # 새 글 작성
 def post_new(request):
+    print("post_new Call !!!!!!")    
+    
+    # 새 글 작성 버튼을 눌렀을 때에는 request가 없으므로 else로 넘어감
+    # form에서 submit하면 현재 URL로 다시 request하므로 여기로 다시옴
     if request.method == "POST":
         form = PostForm(request.POST) # 폼에서 사용자 입력값 가져와서(POST method)
         if form.is_valid(): # 입력값 검증을 함
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now() # 임시저장 기능을 위해 주석
             post.save() # 입력값 저장
             return redirect('post_detail', pk=post.pk) # 상세보기로 화면 전환
+    # 새 글 작성 버튼 클릭 시 
     else:
-        form = PostForm() # forms.py PostForm class
+        form = PostForm() # forms.py PostForm class 가져온 다음 post_edit.html에 매핑시킨다.
+
     return render(request, 'blog/post_edit.html', {'form': form})
+
 
 # 글 수정
 def post_edit(request, pk):
+    print("post_edit222222222222222222 Call !!!!!!")        
     post = get_object_or_404(Post, pk=pk) # Post 모델 인스턴스 가져오기(pk로 SELECT), 수정을 위해 form에 다시 뿌릴것이기 때문
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post) # 기존 글 내용 Post Model에서 가져오기
+        form = PostForm(request.POST, instance=post) # request, 기존 글 내용 Post Model에서 가져오기
         if form.is_valid(): # 입력값 검증
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now() # 임시저장 기능을 위해 주석
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
+
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+# 임시 저장
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+# 게시
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+# 글 삭제
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
